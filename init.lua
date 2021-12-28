f6a9f229706ac9ada0c1bcbb9a6e20b710958d96
Marco Radocchia <marco.radocchia@outlook.com>
2021-12-28 17:28:13 +0100

    language servers configuration

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 require 'user.options'
 require 'user.keymaps'
 require 'user.plugins'
-require 'user.cmp'
 require 'user.colorscheme'
+require 'user.cmp'
+-- The following this is a dir, but contains `init.lua` which is what is sourced
+require 'user.lsp'
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -109,6 +109,16 @@ _G.packer_plugins = {
     path = "/home/radocchia/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-lsp-installer"] = {
+    loaded = true,
+    path = "/home/radocchia/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
+    url = "https://github.com/williamboman/nvim-lsp-installer"
+  },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/home/radocchia/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/radocchia/.local/share/nvim/site/pack/packer/start/packer.nvim",
