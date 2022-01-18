35811c5a4b493909ae9789d7efae5a33c83c1d4a
Marco Radocchia <marco.radocchia@outlook.com>
2022-01-18 19:36:53 +0100

    automatic right space in parenthesis

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -3,6 +3,11 @@ if not status_ok then
 	return
 end
 
+local rule_status_ok, Rule = pcall(require, "nvim-autopairs.rule")
+if not rule_status_ok then
+	return
+end
+
 npairs.setup({
 	check_ts = true,
 	ts_config = {
@@ -27,6 +32,37 @@ npairs.setup({
 		highlight = "PmenuSel",
 		highlight_grey = "LineNr",
 	},
+})
+
+npairs.add_rules({
+	Rule(" ", " "):with_pair(function(opts)
+		local pair = opts.line:sub(opts.col - 1, opts.col)
+		return vim.tbl_contains({ "()", "[]", "{}" }, pair)
+	end),
+	Rule("( ", " )")
+		:with_pair(function()
+			return false
+		end)
+		:with_move(function(opts)
+			return opts.prev_char:match(".%)") ~= nil
+		end)
+		:use_key(")"),
+	Rule("{ ", " }")
+		:with_pair(function()
+			return false
+		end)
+		:with_move(function(opts)
+			return opts.prev_char:match(".%}") ~= nil
+		end)
+		:use_key("}"),
+	Rule("[ ", " ]")
+		:with_pair(function()
+			return false
+		end)
+		:with_move(function(opts)
+			return opts.prev_char:match(".%]") ~= nil
+		end)
+		:use_key("]"),
 })
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
