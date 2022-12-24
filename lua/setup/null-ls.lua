fd5e3c650bc031cd5aaa34bf8cfebb640968fa79
Marco Radocchia <marco.radocchia@outlook.com>
2022-12-24 03:09:47 +0100

    Updated null-ls config

--- lua/setup/null-ls.lua
+++ lua/setup/null-ls.lua
@@ -1,23 +1,9 @@
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
   debug = false,
   sources = {
-    formatting.prettier.with({
-      args = {
-        "--single-quote",
-        "--jsx-single-quote",
-      },
-    }),
-    formatting.black.with({
-      args = {
-        "--line-length",
-        "79",
-        "--fast",
-      },
-    }),
     formatting.latexindent,
     formatting.clang_format.with({
       args = {
